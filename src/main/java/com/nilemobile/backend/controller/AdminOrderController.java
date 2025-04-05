d6384eac967f7b3ef22a6aadf08f445d6c35ab5b
Cuong <ntancuong0211@gmail.com>
2025-04-05 20:11:05 +0700

    updated

--- src/main/java/com/nilemobile/backend/controller/AdminOrderController.java
+++ src/main/java/com/nilemobile/backend/controller/AdminOrderController.java
@@ -4,13 +4,11 @@ import com.nilemobile.backend.exception.Orderexception;
 import com.nilemobile.backend.model.Order;
 import com.nilemobile.backend.reponse.AdminOrderDTO;
 import com.nilemobile.backend.reponse.OrderDetailDTO;
+import com.nilemobile.backend.reponse.OrderStatusDTO;
 import com.nilemobile.backend.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -100,5 +98,34 @@ public class AdminOrderController {
                 )).collect(toList())
         )).collect(toList());
         return ResponseEntity.ok(orderProfile);
+    }
+
+    @PutMapping("/updateStatus/{orderId}")
+    public ResponseEntity<AdminOrderDTO> updateOrderStatus(@PathVariable Long orderId, @RequestBody OrderStatusDTO request) {
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new Orderexception("Order not found"));
+        order.setStatus(request.getStatus());
+        orderRepository.save(order);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy");
+        AdminOrderDTO orderDTO = new AdminOrderDTO(
+                order.getId(),
+                order.getShippingAddress().getFullName(),
+                order.getShippingAddress().getPhoneNumber(),
+                order.getTotalDiscountPrice(),
+                order.getStatus().toString(),
+                order.getOrderDate().format(formatter),
+                order.getShippingAddress().getFullAddress(),
+                order.getPaymentDetails().getPaymentMethod().toString(),
+                order.getOrderDetails().stream().map(orderDetail -> new OrderDetailDTO(
+                        orderDetail.getId(),
+                        orderDetail.getVariation().getId(),
+                        orderDetail.getVariation().getVariationName(),
+                        orderDetail.getVariation().getImageURL(),
+                        orderDetail.getQuantity(),
+                        orderDetail.getSubtotal(),
+                        orderDetail.getTotalDiscountPrice()
+                )).collect(toList())
+        );
+        return ResponseEntity.ok(orderDTO);
     }
 }
